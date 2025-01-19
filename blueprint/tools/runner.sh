0c24867e7dd58fb0ddcbd2e5043db8e2283a2ad2
Goran Ninkovic <goran@goranninkovic.com>
2025-01-19 16:18:21 +0100

    fix: enhance runner.sh to support execution inside or outside DDEV container

--- blueprint/tools/runner.sh
+++ blueprint/tools/runner.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 ROOT=$(git rev-parse --show-toplevel)
-PROJECT_NAME=$(grep "name: " "$ROOT"/.ddev/config.yaml | head -1 | cut -f 2 -d ' ')
-HOSTNAME=$(hostname)
+IS_DDEV=$(stat -c %d "$ROOT/.ddev" 2>/dev/null || echo 0)
 
 # function that runs a command in the ddev web container or outside of it depending on the hostname
 function run() {
+  if [ "$IS_DDEV" == 1 ]; then
+    run_in_ddev_container "$@"
+  else
+    "$@"
+  fi
+}
+
+function run_in_ddev_container() {
+  PROJECT_NAME=$(grep "name: " "$ROOT"/.ddev/config.yaml | head -1 | cut -f 2 -d ' ')
+  HOSTNAME=$(hostname)
   if [ "$HOSTNAME" == "${PROJECT_NAME}-web" ]; then
     "$@"
   else
