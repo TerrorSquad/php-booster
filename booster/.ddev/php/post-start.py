7ffea68436c8f87c62ddb7e8f24e4bb8dd6c94a4
Goran Ninkovic <goran@goranninkovic.com>
2025-07-14 23:05:26 +0200

    refactor: remove unused functions and variables from post-start script

--- booster/.ddev/php/post-start.py
+++ booster/.ddev/php/post-start.py
@@ -1,37 +1,6 @@
 import os
 import subprocess
 
-FILE = ".ddev/php/xdebug-local.ini"
-
-
-def append_to_file(key, value):
-    """
-    Append a key-value pair to a file
-
-    Args:
-        key (str): The key to append
-        value (str): The value to append
-    """
-    with open(FILE, "a") as f:
-        f.write(f"{key}={value}\n")
-
-
-def get_docker_ip():
-    """
-    Get the IP address of the Docker host
-
-    Returns:
-        str: The IP address of the Docker host
-    """
-
-    result = subprocess.run(
-        ["docker", "network", "inspect", "bridge"], capture_output=True, text=True
-    )
-    for line in result.stdout.splitlines():
-        if "Gateway" in line:
-            return line.split(":")[1].strip().strip(' "')
-    return None
-
 
 def update_nginx_config():
     """
