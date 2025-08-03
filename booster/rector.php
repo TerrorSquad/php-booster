6bd52b9a65d583e353937c3e25fbd854f108ad8e
Goran Ninkovic <goran@goranninkovic.com>
2025-08-03 10:15:06 +0200

    refactor: clean up rector configuration by removing unused rules and imports

--- booster/rector.php
+++ booster/rector.php
@@ -3,29 +3,26 @@
 declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
-use Rector\CodingStyle\Rector\Closure\StaticClosureRector;
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\Cast\RecastingRemovalRector;
 use Rector\Set\ValueObject\SetList;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 
 return RectorConfig::configure()
- ->withPaths([
+    ->withPaths([
         __DIR__ . '/DIRECTORY',
- ])
- ->withSets([
- SetList::CODE_QUALITY,
- SetList::CODING_STYLE,
- SetList::TYPE_DECLARATION,
- SetList::PRIVATIZATION,
- SetList::EARLY_RETURN,
- ])
- ->withRules([
- AddVoidReturnTypeWhereNoReturnRector::class,
- RecastingRemovalRector::class,
- ])
- ->withSkip([
- FlipTypeControlToUseExclusiveTypeRector::class,
- StaticClosureRector::class,
- ])
- ->withPhpSets(php83: true);
+    ])
+    ->withSets([
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::TYPE_DECLARATION,
+        SetList::PRIVATIZATION,
+        SetList::EARLY_RETURN,
+    ])
+    ->withRules([
+        RecastingRemovalRector::class,
+    ])
+    ->withSkip([
+        FlipTypeControlToUseExclusiveTypeRector::class,
+    ])
+    ->withPhpSets(php83: true)
+;
