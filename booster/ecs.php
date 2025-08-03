9ae79797f44bc2f96e2bc170e5c9e1e3365f2888
Goran Ninkovic <goran@goranninkovic.com>
2025-08-03 10:14:04 +0200

    fix: update ecs rules

--- booster/ecs.php
+++ booster/ecs.php
@@ -4,13 +4,16 @@ declare(strict_types=1);
 
 use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
 use PhpCsFixer\Fixer\Operator\BinaryOperatorSpacesFixer;
+use PhpCsFixer\Fixer\Phpdoc\PhpdocToCommentFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return ECSConfig::configure()
     ->withPaths([
         __DIR__ . '/DIRECTORY',
     ])
+    /** @phpstan-ignore argument.type */
     ->withRules([
+            /** @phpstan-ignore class.notFound */
         NoUnusedImportsFixer::class,
     ])
     ->withPreparedSets(
@@ -23,7 +26,13 @@ return ECSConfig::configure()
         psr12: true,
         phpCsFixer: true
     )
+    /** @phpstan-ignore class.notFound,argument.type */
     ->withConfiguredRule(BinaryOperatorSpacesFixer::class, [
         'default' => 'align',
     ])
-    ->withRootFiles();
+    ->withRootFiles()
+    ->withSkip([
+            /** @phpstan-ignore class.notFound */
+        PhpdocToCommentFixer::class,
+    ])
+;
